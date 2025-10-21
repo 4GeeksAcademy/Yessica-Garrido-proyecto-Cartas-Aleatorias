a1eb897c9ed678a7091631c67dc623de089db240
yessigarridowork-blip <yessigarrido.work@gmail.com>
2025-10-21 00:55:55 +0000

    carta de poker

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+const icon = ['♥', '♠', '♣', '♦'];
+const valors = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+
+
+function getRandomCard() {
+  const randomIcon = icon[Math.floor(Math.random() * icon.length)];
+  const randomValor = valors[Math.floor(Math.random() * valors.length)];
+  return { icon: randomIcon, valor: randomValor };
+}
+
+function CambiarTarjeta() {
+  const card = getRandomCard();
+  const iconTop = document.getElementById('icon-top');
+  const valorText = document.getElementById('valors');
+  const iconEnd = document.getElementById('icon-end');
+
+  iconTop.textContent = card.icon;
+  valorText.textContent = card.valor;
+  iconEnd.textContent = card.icon;
+}
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("button");
+  button.addEventListener("click", CambiarTarjeta);
+  CambiarTarjeta();
+
+});
+
+
+window.onload = () => {
+  CambiarTarjeta();
+  setInterval(CambiarTarjeta, 10000)
+  
 };
