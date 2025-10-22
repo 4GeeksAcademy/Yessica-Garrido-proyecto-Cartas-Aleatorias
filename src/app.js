96d29ec54aeb7e929cfa57f3015660939a3dc495
yessigarridowork-blip <yessigarrido.work@gmail.com>
2025-10-22 18:38:55 +0000

    my firt card

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const icon = ['♥', '♠', '♣', '♦'];
 const valors = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
 
 
+
 function getRandomCard() {
   const randomIcon = icon[Math.floor(Math.random() * icon.length)];
   const randomValor = valors[Math.floor(Math.random() * valors.length)];
@@ -17,24 +18,45 @@ function getRandomCard() {
 
 function CambiarTarjeta() {
   const card = getRandomCard();
-  const iconTop = document.getElementById('icon-top');
-  const valorText = document.getElementById('valors');
-  const iconEnd = document.getElementById('icon-end');
+  const icontop = document.getElementById('icon-top');
+  const valorText = document.getElementById('valor');
+  const iconend = document.getElementById('icon-end');
 
-  iconTop.textContent = card.icon;
+  icontop.textContent = card.icon;
   valorText.textContent = card.valor;
-  iconEnd.textContent = card.icon;
+  iconend.textContent = card.icon;
+
+  if (card.icon === '♥' || card.icon === '♦') {
+    icontop.style.color = 'red';
+    iconend.style.color = 'red';
+    valorText.style.color = 'red';
+  } else {
+    icontop.style.color = 'black';
+    iconend.style.color = 'black';
+    valorText.style.color = 'black';
+  }
 }
+ 
+
 document.addEventListener("DOMContentLoaded", () => {
   const button = document.getElementById("button");
-  button.addEventListener("click", CambiarTarjeta);
-  CambiarTarjeta();
+  const timeDisplay = document.getElementById("time");
 
+
+  button.addEventListener("click", CambiarTarjeta);
+
+
+  CambiarTarjeta();
+  
+  let i = 10;
+
+  setInterval(() => {
+    i--;
+    timeDisplay.innerHTML = `Next Card ${i}`
+    if (i === 0) {
+      CambiarTarjeta();
+      i = 10;
+    }
+  }, 1000);
 });
 
-
-window.onload = () => {
-  CambiarTarjeta();
-  setInterval(CambiarTarjeta, 10000)
-  
-};
